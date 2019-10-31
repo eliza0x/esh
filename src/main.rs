d72da34f821728f526e976b70b4d959f03427a3e
eliza0x <me@eliza.link>
2019-10-31 16:59:17 +0900

    コードを整理した

--- src/main.rs
+++ src/main.rs
@@ -10,76 +10,85 @@ use std::io::{self, Write};
 use std::process::exit;
 
 lazy_static! {
-    pub static ref BUILDIN_FUNCTIONS: HashMap<&'static str, fn(Vec<&str>) -> i32> = [
-        ("cd",   buildin_cd   as fn(Vec<&str>) -> i32),
-        ("help", buildin_help as fn(Vec<&str>) -> i32),
-        ("exit", buildin_exit as fn(Vec<&str>) -> i32)
+    pub static ref BUILDIN_FUNCTIONS: HashMap<&'static str, fn(&Vec<&str>)> = [
+        ("cd",   buildin_cd   as fn(&Vec<&str>)),
+        ("help", buildin_help as fn(&Vec<&str>)),
+        ("exit", buildin_exit as fn(&Vec<&str>))
     ].iter().cloned().collect();
 }
 
-fn buildin_cd(args: Vec<&str>) -> i32 {
+fn buildin_cd(args: &Vec<&str>) {
     if args.len() < 2 {
         println!("lsh: expected argument to \"cd\"");
-        return 1;
+        return;
     }
     match chdir(args[1]) {
-        Ok(_) => println!("moved");
-        Err(_) => println!("move failed");
+        Ok(_) => println!("moved"),
+        Err(_) => println!("move failed")
     }
-    return 1
 }
 
-fn buildin_help(_args: Vec<&str>) -> i32 {
+fn buildin_help(_args: &Vec<&str>) {
     println!("esh v0.01");
     BUILDIN_FUNCTIONS.iter().for_each(|(name, _)| {
         println!("- {}", name)
     });
-    return 1
 }
 
-fn buildin_exit(_args: Vec<&str>) -> i32 {
-    return 0
+fn buildin_exit(_args: &Vec<&str>) {
+    exit(0);
 }
 
-fn esh_launch(args: Vec<&str>) -> i32 {
-    fn wait_loop(pid: Pid) { // pidのプロセスが終了するまでループ
-        match waitpid(pid, Some(WaitPidFlag::WUNTRACED)) {
-            Ok(WaitStatus::Exited(_, _)) => println!("exited"),
-            Ok(WaitStatus::Signaled(_, _, _)) => println!("signaled"),
-            _ => wait_loop(pid),
-        }
+fn wait_loop(pid: Pid) -> Result<i32, String> { // pidのプロセスが終了するまでループ
+    match waitpid(pid, Some(WaitPidFlag::WUNTRACED)) {
+        Ok(WaitStatus::Exited(_, status)) => Ok(status),
+        Ok(WaitStatus::Signaled(_, _, _)) => Err("signaled".to_string()),
+        _ => wait_loop(pid),
     }
+}
 
+fn esh_launch(args: &Vec<&str>) {
+    let argsc: Vec<CString> = args.iter().map(|&arg|{ CString::new(arg).unwrap() }).collect();
+    if let Err(_) = execvp(&argsc[0], &argsc) {
+        println!("execvp failed.");
+    }
+}
+
+fn esh_execute(commands: Vec<Vec<&str>>) -> Result<i32, String> {
+    let commands_index: usize = 0;
+    let command: &Vec<&str> = &commands[commands_index];
     match fork() {
         Ok(ForkResult::Parent {child, .. }) => {
             wait_loop(child)
         }
         Ok(ForkResult::Child) => {
-            let argsc: Vec<CString> = args.iter().map(|&arg|{ CString::new(arg).unwrap() }).collect();
-            execvp(&argsc[0], &argsc);
+            match BUILDIN_FUNCTIONS.get(command[0]) {
+             Some(f) => f(command),
+             None => esh_launch(command),
+            }
             exit(1);
         }
-        Err(_) => println!("fork failed"),
-    }
-    return 1
-}
-
-fn esh_execute(args: Vec<&str>) -> i32 {
-    if args.len() == 0 { return 1 };
-    match BUILDIN_FUNCTIONS.get(args[0]) {
-     Some(f) => f(args),
-     None => esh_launch(args),
+        Err(_) => Err("fork failed".to_string()),
     }
 }
 
 fn esh_loop() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    let mut line = String::new();
-    let _ = io::stdin().read_line(&mut line);
-    let args: Vec<&str> = line.split_whitespace().collect();
-    let status: i32     = esh_execute(args);
-    if status != 0 { esh_loop(); }
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut line = String::new();
+        let _ = io::stdin().read_line(&mut line);
+        let commands: Vec<Vec<&str>> = line.split("|").map(|c| c.split_whitespace().collect()).collect();
+        let status: Result<i32, String> = esh_execute(commands);
+        match status {
+            Ok(status) => {
+                println!("{}", status);
+            }
+            Err(e) => {
+                println!("{}", e);
+            }
+        }
+    }
 }
 
 fn main() {
